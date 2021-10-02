504dc5be0d65ccf4cb8fcafb62009ee258a0ed43
JM911 <91401126+JM911@users.noreply.github.com>
2021-10-02 12:40:38 +0900

    21. 10. 01.

---
+++ [1000-1999]/[1000-1099]/1010.cpp
@@ -0,0 +1,35 @@
+// 1010. 다리 놓기
+// S1
+
+#include <iostream>
+using namespace std;
+
+long long Combi(int r, int n)
+{
+    long long ans = 1;
+
+    for (int i = 1; i <= r; i++)
+    {
+        ans *= n - i + 1;
+        ans /= i;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    int T, N, M;
+    cin >> T;
+
+    while (T > 0)
+    {
+        cin >> N >> M;
+
+        cout << Combi(N, M) << endl;
+
+        T--;
+    }
+
+    return 0;
+}
---
+++ [1000-1999]/[1400-1499]/1402.cpp
@@ -0,0 +1,25 @@
+// 1402. 아무래도이문제는A번난이도인것같다
+// B1
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    int T;
+    ll A, B;
+
+    cin >> T;
+    cin >> A >> B;
+
+    while (T > 0)
+    {
+        cout << "yes" << endl;
+        T--;
+    }
+
+    return 0;
+}
